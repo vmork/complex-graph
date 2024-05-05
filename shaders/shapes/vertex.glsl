77af2cb992d0532b98972a30717aae4dfbde0020
vmork <vallemork@gmail.com>
2024-05-05 17:29:39 +0200

    add more settings to shapes
    make it possible to create vertices inside a filled circle

--- shaders/shapes/vertex.glsl
+++ shaders/shapes/vertex.glsl
@@ -7,6 +7,7 @@ in vec2 a_position;
 //#UFUNC
 
 void main() { 
+    gl_PointSize = max(u_pointSize, u_pointSize / u_scale.y);
     vec2 pos = mix(a_position, f(a_position), u_t);
     pos = (u_worldMatInv * vec3(pos, 1.0)).xy;
     gl_Position = vec4(pos, 0.0, 1.0);
