1e0b2ef83f41d49dbe9217c3c3d26b8e82faa992
vmork <vallemork@gmail.com>
2024-05-09 16:25:43 +0200

    add server for railway deployment

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at port ${port}`);
 });
